b860fcd0fd07c1533f03546ad206f08420536240
AfinFauzanGhazali <Afin19octo02@gmail.com>
2022-06-14 11:57:56 +0700

    Tugas 7

---
+++ app/src/main/java/com/example/mysqlite__afinfauzanghazali_2011500020/MataKuliah.kt
@@ -0,0 +1,9 @@
+package com.example.mysqlite__afinfauzanghazali_2011500020
+
+class MataKuliah  (
+    var kdMatkul: String,
+    var nmMatkul: String,
+    var sks :Int,
+    var sifat :String
+
+)
